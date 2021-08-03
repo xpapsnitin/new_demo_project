7851acf2eae069c2321a4f3fa76d5fbcf5dbda09
Swamiji <nitinpatilat@gmail.com>
2021-08-04 05:12:50 +0530

    new changes

--- src/com/git/demo/HelloWorld.java
+++ src/com/git/demo/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Git repository!!!! Jenkins Integration !!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Check jenkins build on Git push");
 	}
 
 }
